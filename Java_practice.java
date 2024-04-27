dbdf53be86efe6d6d3e0c9b17f3719393bf6c299
yuyunaginagi <centrepg8@gmail.com>
2024-04-27 14:17:10 +0900

    配列とリストの学習

--- Java_practice.java
+++ Java_practice.java
@@ -8,3 +8,22 @@ class Main {
     System.out.println(((Object)radius).getClass().getSimpleName());
   }
 }
+
+// 配列とリスト
+import java.util.ArrayList;
+
+class Main {
+  public static void main(String[] args) {
+    ArrayList<Integer> scores = new ArrayList<Integer>();
+
+      scores.add(1);
+      scores.add(5);
+      scores.add(10);
+      scores.add(15);
+
+    System.out.println(scores.get(0));
+    System.out.println(scores.get(1));
+    System.out.println(scores.get(2));
+    System.out.println(scores.get(3));
+  }
+}
