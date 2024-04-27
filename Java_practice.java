3a8d4580180df3e3e50a601d26e3ff4d202e2684
yuyunaginagi <centrepg8@gmail.com>
2024-04-27 12:53:26 +0900

    Javaの記述練習

---
+++ Java_practice.java
@@ -0,0 +1,10 @@
+// Javaの記述練習
+
+class Main {
+  public static void main(String[] args) {
+    var radius = 5;
+    System.out.println(radius * radius * 3.14);
+
+    System.out.println(((Object)radius).getClass().getSimpleName());
+  }
+}
