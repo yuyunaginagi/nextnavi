4b2d12b9bef97bbd8d179d8f958b0b58dfd7e296
yuyunaginagi <centrepg8@gmail.com>
2024-04-27 14:45:38 +0900

    メソッドの学習

--- Java_practice.java
+++ Java_practice.java
@@ -56,3 +56,15 @@ class Main {
     }
   }
 }
+
+// メソッド
+class Main {
+  public static void main(String[] args) {
+    var answer = square(5);
+    System.out.println(answer);
+  }
+
+  public static int square(int number) {
+    return number * number;
+  }
+}
