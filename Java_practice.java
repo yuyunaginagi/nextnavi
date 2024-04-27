1fc0c544a4e9f6ac2e12a3f7ed7768ba3ddc7274
yuyunaginagi <centrepg8@gmail.com>
2024-04-27 14:32:06 +0900

    拡張for文の学習

--- Java_practice.java
+++ Java_practice.java
@@ -38,3 +38,21 @@ class Main {
     }
   }
 }
+
+// 拡張for文
+import java.util.ArrayList;
+
+class Main {
+  public static void main(String[] args) {
+    ArrayList<Integer> scores = new ArrayList<Integer>();
+    
+    scores.add(1);
+    scores.add(5);
+    scores.add(10);
+    scores.add(15);
+
+    for(int score : scores) {
+      System.out.println(score);
+    }
+  }
+}
